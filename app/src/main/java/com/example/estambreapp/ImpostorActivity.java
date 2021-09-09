a09ad0a48126b0e238e3ba24c202211e3f1c97da
Daniel-dadu <67434516+Daniel-dadu@users.noreply.github.com>
2021-09-08 19:49:52 -0500

    Added button matrix in Impostor

--- app/src/main/java/com/example/estambreapp/ImpostorActivity.java
+++ app/src/main/java/com/example/estambreapp/ImpostorActivity.java
@@ -3,13 +3,48 @@ package com.example.estambreapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class ImpostorActivity extends AppCompatActivity {
+
+    TableLayout tableButtons;
+    ImpostorModel impostorModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_impostor);
+
+        tableButtons = (TableLayout) findViewById(R.id.impostorTableButtons);
+        createTableButtons();
+    }
+
+    private void createTableButtons() {
+        // --- Error, no accede a la clase ---
+        int numRows = 2; // (int) impostorModel.getTableButtonsSize()[0];
+        int numColumns = 3; // (int) impostorModel.getTableButtonsSize()[1];
+
+        for(int i = 0; i < numRows; i++){
+            TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    1.0f
+            )); // Making the row expand and use the complete size of the horizontal space
+            tableButtons.addView(tableRow); // Creating a new row in the table
+            for(int j = 0; j < numColumns; j++){
+                Button button = new Button(this);
+                button.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        1.0f
+                )); // Making the column expand and use the complete size of the vertical space
+                tableRow.addView(button);
+            }
+        }
+
     }
 
     @Override
--- app/src/main/java/com/example/estambreapp/ImpostorModel.java
+++ app/src/main/java/com/example/estambreapp/ImpostorModel.java
@@ -1,4 +1,9 @@
 package com.example.estambreapp;
 
 public class ImpostorModel {
+
+
+    public int[] getTableButtonsSize(){
+        return new int[]{ 2,3 }; // Hardcoded for experimentation purposes
+    }
 }
