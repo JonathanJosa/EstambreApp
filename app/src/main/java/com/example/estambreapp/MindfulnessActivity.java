5e501b517edfe6865396d498ffe5d5f6c7b63e6d
danoman17 <daniflores374@gmail.com>
2021-09-10 01:34:54 -0500

    init musicController

--- app/src/main/java/com/example/estambreapp/MindfulnessActivity.java
+++ app/src/main/java/com/example/estambreapp/MindfulnessActivity.java
@@ -2,16 +2,34 @@ package com.example.estambreapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 
 public class MindfulnessActivity extends AppCompatActivity {
 
+    public MediaPlayer mediaPlayer;
+    SongsApiManager songsApiManager;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mindfulness);
+
+
+        mediaPlayer = new MediaPlayer();
+
+        Context context = getApplicationContext();
+
+        songsApiManager.prepareMediaPlayer(mediaPlayer, context);
+        mediaPlayer.start();
+
+
+
     }
 
     @Override
--- app/src/main/java/com/example/estambreapp/SongsApiManager.java
+++ app/src/main/java/com/example/estambreapp/SongsApiManager.java
@@ -1,3 +1,94 @@
 package com.example.estambreapp;
 
-public class SongsApiManager {  }
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.widget.Toast;
+
+public class SongsApiManager {
+
+    int musicIndex = 0;
+    String[] musicUrl = new String[]{
+
+            "https://firebasestorage.googleapis.com/v0/b/estambreapp.appspot.com/o/1.mp3?alt=media&token=a0dfb33a-6add-4fae-8ed9-a203e3644b66",
+            "https://firebasestorage.googleapis.com/v0/b/estambreapp.appspot.com/o/2.mp3?alt=media&token=ea3dc64f-ddeb-4606-8461-e9dc2312634f",
+            "https://firebasestorage.googleapis.com/v0/b/estambreapp.appspot.com/o/3.mp3?alt=media&token=201d5b36-e1d9-4e20-8a1a-ea6c6da18275"
+    };
+
+
+
+    public void prepareMediaPlayer(MediaPlayer mediaPlayer, Context context) {
+        try {
+
+            mediaPlayer.setDataSource(musicUrl[ musicIndex ]); // URL of music file
+            mediaPlayer.prepare();
+        } catch (Exception exception) {
+            Toast.makeText(context,exception.getMessage(), Toast.LENGTH_SHORT).show(); // mostramos mensaje pequeño de lo que salio mal
+
+        }
+    }
+
+    public void nextSong( MediaPlayer mediaPlayer, Context context){
+
+        musicIndex = calculateIndexNextSong( musicIndex++ );
+
+
+        if(mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.reset();
+            prepareMediaPlayer(mediaPlayer, context);
+            mediaPlayer.start();
+        } else {
+            mediaPlayer.reset();
+            prepareMediaPlayer(mediaPlayer, context);
+            mediaPlayer.start();
+            //imagePlayPause.setImageResource(R.drawable.ic_baseline_pause);
+        }
+
+    }
+
+    public void  prevSong(MediaPlayer mediaPlayer, Context context){
+
+        if(mediaPlayer.isPlaying() && mediaPlayer.getCurrentPosition() <= 5000 ){
+
+            musicIndex = calculateIndexPrevSong( musicIndex-- );
+            mediaPlayer.stop();
+            mediaPlayer.reset();
+            prepareMediaPlayer( mediaPlayer, context );
+            mediaPlayer.start();
+
+        }
+        else if (mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+            mediaPlayer.seekTo(0);
+            mediaPlayer.start();
+        }
+        else {
+            musicIndex = calculateIndexPrevSong( musicIndex-- );
+            mediaPlayer.reset();
+            prepareMediaPlayer( mediaPlayer, context );
+            mediaPlayer.start();
+            //imagePlayPause.setImageResource(R.drawable.ic_baseline_pause);
+        }
+    }
+
+
+    public int calculateIndexNextSong( int index ) {
+
+        if( index >= musicUrl.length -1 ){
+            return 0;
+        }else {
+            return  index + 1;
+        }
+
+    }
+
+    public int calculateIndexPrevSong( int index ){
+        if( index <= 0){
+            return musicUrl.length - 1;
+        } else {
+            return index - 1;
+        }
+    }
+
+
+}
