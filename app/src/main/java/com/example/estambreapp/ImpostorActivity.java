2f15fbfb576394cc221afe84862dde9284aa3ebf
Daniel-dadu <67434516+Daniel-dadu@users.noreply.github.com>
2021-09-10 16:22:38 -0500

    Deleted unused imports

--- app/src/main/java/com/example/estambreapp/ImpostorActivity.java
+++ app/src/main/java/com/example/estambreapp/ImpostorActivity.java
@@ -1,24 +1,12 @@
 package com.example.estambreapp;
 
-import androidx.annotation.ColorInt;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.ColorFilter;
 import android.graphics.Typeface;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
