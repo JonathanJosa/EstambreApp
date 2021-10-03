9cf2c0b49477c163a735cab6f44d6a594302993b
Daniel-dadu <67434516+Daniel-dadu@users.noreply.github.com>
2021-10-03 00:04:13 -0500

    Added elements to the xml on infinityMaze

--- app/src/main/java/com/example/estambreapp/InfinityMazeActivity.java
+++ app/src/main/java/com/example/estambreapp/InfinityMazeActivity.java
@@ -2,15 +2,52 @@ package com.example.estambreapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.Toast;
 
 public class InfinityMazeActivity extends AppCompatActivity {
+
+    TableLayout mazeTable;
+    InfinityMazeModel infinityMazeModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infinity_maze);
+
+        infinityMazeModel = new InfinityMazeModel();
+
+        mazeTable = findViewById(R.id.mazeTable);
+        createMazeTable();
     }
+
+    private void createMazeTable(){
+        boolean[][] mazeMatrix = infinityMazeModel.getMazeMatrix();
+
+    }
+
+    public void onArrowClicked(View v){ // Everytime an arrow is clicked, this function is called
+        int viewID = v.getId();
+        if(viewID == R.id.arrowUpBtn){
+            // move up
+            Toast.makeText(this, "Moving up", Toast.LENGTH_SHORT).show();
+        } else if(viewID == R.id.arrowDownBtn) {
+            //move down
+            Toast.makeText(this, "Moving down", Toast.LENGTH_SHORT).show();
+        } else if(viewID == R.id.arrowRightBtn) {
+            // move right
+            Toast.makeText(this, "Moving right", Toast.LENGTH_SHORT).show();
+        } else if(viewID == R.id.arrowLeftBtn) {
+            // move left
+            Toast.makeText(this, "Moving left", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // If I try to go back to the game instructions, the app crashes
+    public void exitGame(View _v){ startActivity(new Intent(this, GamesHomeActivity.class)); }
 
     @Override
     public void onWindowFocusChanged(boolean focused){
--- app/src/main/java/com/example/estambreapp/InfinityMazeModel.java
+++ app/src/main/java/com/example/estambreapp/InfinityMazeModel.java
@@ -1,4 +1,20 @@
 package com.example.estambreapp;
 
 public class InfinityMazeModel {
+
+    public int[] getMazeTableSize(){
+        return new int[]{7, 7};
+    }
+
+    public boolean[][] getMazeMatrix() {
+        return new boolean[][] {
+                {false, true, false, false, false, false},
+                {false, true, false, true, true, true},
+                {false, true, false, true, false, true},
+                {false, true, false, true, false, true},
+                {false, true, false, true, false, true},
+                {false, true, true, true, false, true},
+                {false, false, false, false, false, true}
+        };
+    }
 }
