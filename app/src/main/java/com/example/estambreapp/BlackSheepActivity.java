645e7723848371e4f9734df26e2c5201fe0c8df2
JonathanJosa <A01734225@itesm.mx>
2021-10-12 13:54:39 -0500

    functionality completed

--- app/src/main/java/com/example/estambreapp/BlackSheepActivity.java
+++ app/src/main/java/com/example/estambreapp/BlackSheepActivity.java
@@ -1,26 +1,39 @@
 package com.example.estambreapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.transition.AutoTransition;
 
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.transition.Scene;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class BlackSheepActivity extends AppCompatActivity{
 
     BlackSheepModel sheepModel;
-    LinearLayout board;
+    RelativeLayout board;
+    TextView instruccion;
+    int rounds = 3;
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_black_sheep);
         board = findViewById(R.id.LinearLayoutBoard);
+        instruccion = findViewById(R.id.restante);
         sheepModel  = new BlackSheepModel(this);
         createBoard();
     }
@@ -31,23 +44,42 @@ public class BlackSheepActivity extends AppCompatActivity{
         if(focused) new ScreenConfig(getWindow().getDecorView());
     }
 
+    private void nextRound(){
+        instruccion.setText("Bien hecho!!!");
+        sheepModel.playing();
+        sheepModel.started = false;
+        rounds -= 1;
+        Toast.makeText(this, "Ganaste Round!!", Toast.LENGTH_SHORT).show();
+        board.removeAllViews();
+        if(rounds == 0){
+            (new Handler()).postDelayed(this::endGame, 1000);
+            return;
+        }
+        sheepModel.resetGame();
+        (new Handler()).postDelayed(this::createBoard, 1000);
+    }
+
     private ImageButton setButtonsSettings(ImageButton btn, String btnType){
         btn.setContentDescription(btnType);
         btn.setImageResource(getResources().getIdentifier("blacksheep_"+btnType, "drawable", getPackageName()));
         btn.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        btn.setLayoutParams(sheepModel.randomLayouts());
         btn.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#00FFFFFF")));
-        btn.setOnClickListener((View v) -> sheepModel.sheepClicked(v, btnType));
+        btn.setOnClickListener((View v) -> {if(sheepModel.sheepClicked(v, btnType)) nextRound();});
+        btn.setLayoutParams(sheepModel.randomLayouts());
         return btn;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     private void createBoard(){
-        for(int i=0; i<sheepModel.sheeps.first; i++){
-            board.addView(setButtonsSettings(new ImageButton(this), "sheepw"));
-        }
-        for(int i=0; i<sheepModel.sheeps.second; i++){
-            board.addView(setButtonsSettings(new ImageButton(this), "sheepb"));
-        }
+        instruccion.setText("Mezclando...");
+        for(int i=0; i<sheepModel.whiteSheeps; i++){ board.addView(setButtonsSettings(new ImageButton(this), "sheepw")); }
+        for(int i=0; i<sheepModel.blackSheeps; i++){ board.addView(setButtonsSettings(new ImageButton(this), "sheepb")); }
+        for(View btn: board.getTouchables()){ sheepModel.mixSheeps(btn, sheepModel.totalTime); }
+        (new Handler()).postDelayed(() -> {
+            sheepModel.started = true;
+            sheepModel.playing();
+            instruccion.setText("Encuentra la oveja negra!!");
+        }, (long) sheepModel.velocity * sheepModel.totalTime);
     }
 
     private void endGame(){
