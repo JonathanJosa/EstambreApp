e403a429ab4a2ce43e7fec25401a05e7fe9d8641
Daniel-dadu <67434516+Daniel-dadu@users.noreply.github.com>
2021-10-20 14:19:47 -0500

    Added about activity

---
+++ app/src/main/java/com/example/estambreapp/AboutActivity.java
@@ -0,0 +1,13 @@
+package com.example.estambreapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
